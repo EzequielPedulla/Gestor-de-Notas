26e7eba3f06a9c64d6148c7cdb1e0e695cd3cb36
Ezequiel Pedulla <ivanpedulla@gmail.com>
2024-02-15 11:37:37 -0300

    se oculto contraseña usuarios

--- usuarios/usuario.py
+++ usuarios/usuario.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import datetime
-
+import hashlib
 database = mysql.connector.connect(
 
     host='localhost',
@@ -26,13 +26,22 @@ class Usuario:
 
     def registrar(self):
         fecha = datetime.datetime.now()
+        # cifrar contraseña
+        cifrado = hashlib.sha256()
+        # el valor debe ser en bytes
+        cifrado.update(self.password.encode('utf8'))
+
         sql = 'INSERT INTO usuarios VALUES(null,%s,%s,%s,%s,%s)'
         usuario = (self.nombre, self.apellidos,
-                   self.email, self.password, fecha)
+                   self.email, cifrado.hexdigest(), fecha)
+        try:
+            cursor.execute(sql, usuario)
+            database.commit()
+            result = [cursor.rowcount, self]
+        except:
+            result = [0, self]
 
-        cursor.execute(sql, usuario)
-        database.commit()
-        return [cursor.rowcount, self]
+        return result
 
     def identificar(self):
         pass
